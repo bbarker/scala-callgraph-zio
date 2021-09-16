d8611d14898b390e0971d96942adf64189413407
Brandon Barker <brabarker@williamhill.us>
2021-09-16 14:05:05 -0400

    fixed an import issue; now compiles

--- callgraph/core/src/callgraph/zio/ZIOCallGraph.scala
+++ callgraph/core/src/callgraph/zio/ZIOCallGraph.scala
@@ -1,13 +1,14 @@
 /*
 
-*/
+ */
 
 package callgraph.zio
 
-import callgraph.*
+import callgraph.{CallGraph, CallGraphEvaluator}
+import scala.compiletime.uninitialized as ???
 import zio.*
 
 object ZIOCallGraphEvaluator extends CallGraphEvaluator:
-    // for reference, see FiberContext.evaluateNow
-    // e.g. https://github.com/zio/zio/blob/master/core/shared/src/main/scala/zio/internal/FiberContext.scala#L275
-    def evaluateNow(io0: IO[E, Any]): UIO[CallGraph] = UIO.succeed(new CallGraph)
+  // for reference, see FiberContext.evaluateNow
+  // e.g. https://github.com/zio/zio/blob/master/core/shared/src/main/scala/zio/internal/FiberContext.scala#L275
+  def evaluateNow[E](io0: IO[E, Any]): UIO[CallGraph] = UIO.succeed(new CallGraph {})
