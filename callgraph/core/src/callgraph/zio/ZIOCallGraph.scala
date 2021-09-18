9b904196c1f0510c64f801c3c480a9f839bef1f3
Brandon Barker <beb82@cornell.edu>
2021-09-18 09:30:24 -0400

    skeleton macro compiles

--- callgraph/core/src/callgraph/zio/ZIOCallGraph.scala
+++ callgraph/core/src/callgraph/zio/ZIOCallGraph.scala
@@ -13,10 +13,15 @@ object ZIOCallGraphEvaluator extends CallGraphEvaluator:
   type ZioAny = ZIO[Any, Any, Any]
   // for reference, see FiberContext.evaluateNow
   // e.g. https://github.com/zio/zio/blob/master/core/shared/src/main/scala/zio/internal/FiberContext.scala#L275
-  inline def evaluateNow(io0: Expr[ZioAny]): UIO[CallGraph] = ${
+/*   inline def evaluateNow(io0: Expr[ZioAny]): UIO[CallGraph] = ${
     uioSucceed(valName(io0))
   }
-
   def uioSucceed(name: Expr[String])(using Quotes): Expr[UIO[CallGraph]] = '{ UIO.succeed(new CallGraph(${name}) {}) }
 
-  def valName(value: Expr[Any])(using Quotes): Expr[String] = Expr(value.show)
+  def valName(value: Expr[Any])(using Quotes): Expr[String] = Expr(value.show)  
+ */
+
+  inline def evaluateNow(inline io0: ZioAny): UIO[CallGraph] = ${evaluateNowImpl('io0)}  
+
+  def evaluateNowImpl(expr: Expr[ZioAny])(using Quotes): Expr[UIO[CallGraph]] = '{ UIO.succeed(new CallGraph(${Expr(expr.show)}) {}) }
+
